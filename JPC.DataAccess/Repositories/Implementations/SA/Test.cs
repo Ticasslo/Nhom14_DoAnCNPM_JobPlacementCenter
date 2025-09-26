90a8bbee30048530f63774c9cdcd82d0039247a6
Ticasslo <win2005thang@gmail.com>
2025-09-26 11:36:35 +0700

    Test

---
+++ JPC.DataAccess/Repositories/Implementations/SA/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPC.DataAccess.Repositories.Implementations.SA
+{
+    internal class Test
+    {
+    }
+}
