4d38dcee831936b338801e0edffca7efec684fe3
NhatKha <phamkha2021.qng@gmail.com>
2025-10-18 23:08:02 +0700

    Chinh UI ERS

--- JPC.WinForms/Forms/ERS/DangTinTuyenDung_UC_Form.Designer.cs
+++ JPC.WinForms/Forms/ERS/DangTinTuyenDung_UC_Form.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnBack = new System.Windows.Forms.PictureBox();
             this.label9 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -54,7 +54,6 @@
             this.txtkhuvuc = new Guna.UI2.WinForms.Guna2TextBox();
             this.btndangtin = new Guna.UI2.WinForms.Guna2Button();
             this.guna2AnimateWindow1 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
-            this.txtmota = new Guna.UI2.WinForms.Guna2TextBox();
             this.dgvTinTuyenDung = new Guna.UI2.WinForms.Guna2DataGridView();
             this.label12 = new System.Windows.Forms.Label();
             this.dtphannop = new Guna.UI2.WinForms.Guna2DateTimePicker();
@@ -62,6 +61,9 @@
             this.label13 = new System.Windows.Forms.Label();
             this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
             this.numkinhnghiem1 = new Guna.UI2.WinForms.Guna2NumericUpDown();
+            this.guna2AnimateWindow2 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
+            this.guna2AnimateWindow3 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
+            this.txtmota = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.btnBack)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numkinhnghiem)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvTinTuyenDung)).BeginInit();
@@ -358,48 +360,29 @@
             this.btndangtin.Text = "Đăng tin";
             this.btndangtin.Click += new System.EventHandler(this.btndangtin_Click);
             // 
-            // txtmota
-            // 
-            this.txtmota.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.txtmota.DefaultText = "";
-            this.txtmota.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.txtmota.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.txtmota.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.txtmota.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.txtmota.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtmota.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.txtmota.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtmota.Location = new System.Drawing.Point(214, 157);
-            this.txtmota.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.txtmota.Name = "txtmota";
-            this.txtmota.PlaceholderText = "";
-            this.txtmota.SelectedText = "";
-            this.txtmota.Size = new System.Drawing.Size(229, 35);
-            this.txtmota.TabIndex = 176;
-            // 
             // dgvTinTuyenDung
             // 
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
-            this.dgvTinTuyenDung.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dgvTinTuyenDung.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvTinTuyenDung.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(238)))), ((int)(((byte)(243)))));
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvTinTuyenDung.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvTinTuyenDung.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dgvTinTuyenDung.ColumnHeadersHeight = 4;
             this.dgvTinTuyenDung.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvTinTuyenDung.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvTinTuyenDung.DefaultCellStyle = dataGridViewCellStyle3;
             this.dgvTinTuyenDung.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.dgvTinTuyenDung.Location = new System.Drawing.Point(1090, 118);
             this.dgvTinTuyenDung.Name = "dgvTinTuyenDung";
@@ -481,6 +464,7 @@
             // 
             this.guna2CustomGradientPanel1.BackColor = System.Drawing.Color.Transparent;
             this.guna2CustomGradientPanel1.BorderRadius = 30;
+            this.guna2CustomGradientPanel1.Controls.Add(this.txtmota);
             this.guna2CustomGradientPanel1.Controls.Add(this.numkinhnghiem1);
             this.guna2CustomGradientPanel1.Controls.Add(this.txttieude);
             this.guna2CustomGradientPanel1.Controls.Add(this.label3);
@@ -495,7 +479,6 @@
             this.guna2CustomGradientPanel1.Controls.Add(this.label11);
             this.guna2CustomGradientPanel1.Controls.Add(this.txtkhuvuc);
             this.guna2CustomGradientPanel1.Controls.Add(this.cbovitrichuyenmon);
-            this.guna2CustomGradientPanel1.Controls.Add(this.txtmota);
             this.guna2CustomGradientPanel1.Controls.Add(this.numkinhnghiem);
             this.guna2CustomGradientPanel1.Controls.Add(this.cbonhomnghe);
             this.guna2CustomGradientPanel1.Controls.Add(this.cbohinhthuc);
@@ -524,6 +507,14 @@
             this.numkinhnghiem1.Name = "numkinhnghiem1";
             this.numkinhnghiem1.Size = new System.Drawing.Size(229, 34);
             this.numkinhnghiem1.TabIndex = 181;
+            // 
+            // txtmota
+            // 
+            this.txtmota.Location = new System.Drawing.Point(214, 148);
+            this.txtmota.Name = "txtmota";
+            this.txtmota.Size = new System.Drawing.Size(229, 45);
+            this.txtmota.TabIndex = 182;
+            this.txtmota.Text = "";
             // 
             // DangTinTuyenDung_UC_Form
             // 
@@ -570,7 +561,6 @@
         private Guna.UI2.WinForms.Guna2TextBox txtkhuvuc;
         private Guna.UI2.WinForms.Guna2Button btndangtin;
         private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow1;
-        private Guna.UI2.WinForms.Guna2TextBox txtmota;
         private Guna.UI2.WinForms.Guna2DataGridView dgvTinTuyenDung;
         private System.Windows.Forms.Label label12;
         private Guna.UI2.WinForms.Guna2DateTimePicker dtphannop;
@@ -579,5 +569,8 @@
         private System.Windows.Forms.Label label13;
         private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
         private Guna.UI2.WinForms.Guna2NumericUpDown numkinhnghiem1;
+        private System.Windows.Forms.RichTextBox txtmota;
+        private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow2;
+        private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow3;
     }
 }
